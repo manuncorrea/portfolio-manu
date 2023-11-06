13ca8b8f4cd2c9c10ab7816936e570a20f4254f2
Emanuele Correa <emanuelle@taqe.com.br>
2023-11-06 15:39:23 -0300

    atualizando projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,6 +1,7 @@
 import { IDComponents } from '@/types/IDComponents'
 import repository from '../../data/repository.json'
 import Title from '../Title'
+import Image from 'next/image'
 
 export default function Projects({ id }: IDComponents) {
   return(
@@ -10,8 +11,8 @@ export default function Projects({ id }: IDComponents) {
         {repository.map((e, index) => {
           return (
             <div key={index} className="bg-primary rounded-lg cursor-pointer transition-transform hover:-translate-y-2 hover:shadow-lg w-450 mx-auto mb-8 p-4">
-              <h3 className="text-white text-center w-full">{e.name}</h3>
-              <img src={e.photo} alt={e.name} className="w-full"/>
+              <h3 className="text-white text-center w-full mb-2">{e.name}</h3>
+              <img width={1920} height={970} src={e.photo} alt={e.name} className="w-full"/>
               <section className="mt-4">
                 <div className="flex justify-between px-8">
                   <a href={e.repository} target="_blank" rel="noopener noreferrer" className="bg-purple-500 px-4 py-2 rounded text-white font-semibold border border-transparent hover:border-white">Repositorio</a>
